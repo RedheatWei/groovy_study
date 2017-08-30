a86f2e18380d9629c930f50e61c89248dae6add1
redheatwei <qjyyn@qq.com>
2017-08-30 16:19:11 +0800

    basic syntax

--- for_range.groovy
+++ for_range.groovy
@@ -1,6 +1,25 @@
-def repeat(val){
-    for(i=0;i<=5;i++){
-        println val
+package org.acme.groovy
+
+class Song{
+    def name
+    def artist
+    def genre
+    String toString(){
+        "${name}, ${artist}, ${getGenre()}"
+    }
+    def getGenre(){
+        genre.toUpperCase()
     }
 }
-repeat("hello world")
+def sng = new Song(name:"Le Freak",
+        artist:"Chic", genre:"Disco")
+def sng2 = new Song(name:"Kung Fu Fighting", genre:"Disco")
+println(sng2.artist?.toUpperCase())
+def sng3 = new Song()
+sng3.name = "Funkytown"
+sng3.artist = "Lipps Inc."
+sng3.setGenre("Disco")
+println(sng3.genre)
+assert sng3.getArtist() == "Lipps Inc."
+
+
