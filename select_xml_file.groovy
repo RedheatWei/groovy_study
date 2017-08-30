1e8d6c5f20bcf9edac74b5438a3b86b9e39658b1
redheatwei <qjyyn@qq.com>
2017-08-30 18:15:41 +0800

    check git files

--- select_xml_file.groovy
+++ select_xml_file.groovy
@@ -1,10 +1,18 @@
 //import org.boon.Boon;
+def execute(cmd){
+    def proc = cmd.execute()
+    proc.waitFor()
+}
 def manifest_files = "git@10.240.205.131:nfv/manifests.git"
-def shell = "cd `mktemp -d`&&git clone ${manifest_files}"
-println(shell)
-Process git_clone = shell.execute()
-git_clone.waitFor()
-println(git_clone.text)
+def tmp_shell = "mktemp -d".execute()
+def tmp_dir = tmp_shell.text.split("\n")[0]
+def shell  = "/usr/bin/git clone ${manifest_files} ${tmp_dir}"
+try{
+    def clone_git = shell.execute()
+    println(clone_git.text)
+} catch(Exception e){
+    println e1
+}
 //def jsonEditorOptions = Boon.fromJson(/{
 //        disable_edit_json: true,
 //        disable_properties: true,
