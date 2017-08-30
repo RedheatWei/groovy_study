72ac760c249bf8d93e6a9293543a853716a324a9
redheatwei <qjyyn@qq.com>
2017-08-30 16:36:12 +0800

    check git files

--- select_xml_file.groovy
+++ select_xml_file.groovy
@@ -1,29 +1,29 @@
-import org.boon.Boon;
+//import org.boon.Boon;
 def manifest_files = "git@10.240.205.131:nfv/manifests.git"
-Process tmp_dir = "mktemp -d".execute()
-def shell = "cd ${tmp_dir.text};git clone ${manifest_files}"
+def shell = "cd `mkdir -d`;git clone ${manifest_files}"
 println(shell)
 Process git_clone = shell.execute()
-def jsonEditorOptions = Boon.fromJson(/{
-        disable_edit_json: true,
-        disable_properties: true,
-        no_additional_properties: true,
-        disable_collapse: true,
-        disable_array_add: true,
-        disable_array_delete: true,
-        disable_array_reorder: true,
-        theme: "bootstrap2",
-        iconlib:"fontawesome4",
-        schema: {
-          type: "object",
-          properties: {
-            year: {
-              type: "string",
-              enum: [
-                ${git_clone.text}
-              ],
-              default: 2008
-            }
-          }
-        }
-}/);
+println(git_clone.text)
+//def jsonEditorOptions = Boon.fromJson(/{
+//        disable_edit_json: true,
+//        disable_properties: true,
+//        no_additional_properties: true,
+//        disable_collapse: true,
+//        disable_array_add: true,
+//        disable_array_delete: true,
+//        disable_array_reorder: true,
+//        theme: "bootstrap2",
+//        iconlib:"fontawesome4",
+//        schema: {
+//          type: "object",
+//          properties: {
+//            year: {
+//              type: "string",
+//              enum: [
+//                ${git_clone.text}
+//              ],
+//              default: 2008
+//            }
+//          }
+//        }
+//}/);
