bd0c68e431e770a395421b905766373869e78900
redheatwei <qjyyn@qq.com>
2017-08-31 10:12:52 +0800

    check git files

--- select_xml_file.groovy
+++ select_xml_file.groovy
@@ -3,16 +3,28 @@ def execute(cmd){
     def proc = cmd.execute()
     proc.waitFor()
 }
-def manifest_files = "git@10.240.205.131:nfv/manifests.git"
-def tmp_shell = "mktemp -d".execute()
-def tmp_dir = tmp_shell.text.split("\n")[0]
-def shell  = "/usr/bin/git clone ${manifest_files} ${tmp_dir}"
-def clone_git = shell.execute()
-println(clone_git.text)
-def dir = new File(tmp_dir)
-dir.eachFileMatch{
-    println it.getName()
+def getTmpDir(){
+    def tmp_shell = execute("mktemp -d")
+    def tmp_dir = tmp_shell.text.split("\n")[0]
 }
+def cloneManifest(tmp_dir){
+    def manifest_files = "git@10.240.205.131:nfv/manifests.git"
+    def shell  = "/usr/bin/git clone ${manifest_files} ${tmp_dir}"
+    def clone_git = execute(shell)
+    println(clone_git.text)
+}
+def getXmlFiles(){
+//    def find_shell = "find -name '*.xml'"
+//    println(find_shell)
+//    def file_list = find_shell.execute(null,new File(tmp_dir))
+//    file_list.waitFor()
+    def process=new ProcessBuilder("find -name '*.xml'").redirectErrorStream(true).start()
+    process.inputStream.eachLine {println it}
+}
+tmp_dir = getTmpDir()
+cloneManifest(tmp_dir)
+getXmlFiles()
+
 //def jsonEditorOptions = Boon.fromJson(/{
 //        disable_edit_json: true,
 //        disable_properties: true,
