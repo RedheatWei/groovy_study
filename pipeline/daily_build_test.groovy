0d2f9e69280ba376d7cfb9e09a61e11f4852f934
Redheat <qjyyn@qq.com>
2017-10-30 16:22:45 +0800

    some change for host ip

--- pipeline/daily_build_test.groovy
+++ pipeline/daily_build_test.groovy
@@ -16,7 +16,6 @@ def workspace_dir = "${BUILD_ID}/${manifest_file}/thinkcloud" //在Jenkins里创
 def iso_dir = "/opt/ThinkCloud_iso/${group_name}/${JOB_NAME}/${manifest_file}/${mode}/${time}" //镜像存放目录
 def url = "http://10.100.218.203:8099/${group_name}/${JOB_NAME}/${manifest_file}/${mode}/${time}"
 def iso_name = "LenovoOpenStack-nfv_dev_4.1T_${mode}_${time}_${BUILD_ID}.iso"
-def iso_url = url+"/"+iso_name
 
 def DEPLOY_MODE = "ha"
 def DEPLOY_NET = "net1"
@@ -50,22 +49,41 @@ pipeline {
             }
         }
         stage('scp iso'){
-            steps{
-                sh "scp ${iso_dir}/${iso_name} root@10.100.218.150:/tmp/daily && ssh root@10.100.218.150 'echo ${iso_name} ${DEPLOY_MODE} ${DEPLOY_NET} ${iso_url} ${MANIFESTS} >> /tmp/daily/iso_name.log'"
+            steps {
+                sh "scp ${iso_dir}/${iso_name} root@10.100.218.150:/tmp/daily && ssh root@10.100.218.150 'echo ${iso_name} ${DEPLOY_MODE} ${DEPLOY_NET} >> /tmp/daily/iso_name.log'"
+                script{
+                    try{
+                        sh script: "ssh root@10.100.218.150 'cd /tmp/Auto_TC-2.0/bin/;sh run.sh /tmp/daily/${iso_name} /tmp/ThinkCloud_license_trial_ha_ceph_till20171231.zip net4 daily'"
+                        def ha_status = 'SUCCESS'
+                    }catch (e){
+                        def ha_status = 'FAILURE'
+                    }
+                    try{
+                        sh script: "ssh root@10.100.218.150 'cd /tmp/Auto_TC-2.0/bin/;sh run.sh /tmp/daily/${iso_name} /tmp/ThinkCloud_license_trial_multinode_ceph_till20171231.zip net5 daily'"
+                        def multinode_status = 'SUCCESS'
+                    }catch (e){
+                        def multinode_status = 'FAILURE'
+                    }
+                }
+
             }
         }
     }
     post {
-        failure {
-            mail (to: "weipeng4@lenovo.com,cuixf1@lenovo.com",
-                    subject: "Jenkins Auto Mail:${JOB_NAME}-${MANIFESTS} build FAILURE!",
-                    body: "The job ${JOB_NAME}-${BUILD_ID}-${MANIFESTS} BUILD ERROR.");
+        always {
+            emailext (
+                    subject: "Jenkins Build Report",
+                    body: """
+Not HA auto deploy env:
+Auto deploy success: ${multinode_status} 
+
+HA auto deploy env: 
+Auto deploy success: ${ha_status} 
+                """,
+                    to: "weipeng4@lenovo.com",
+                    recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+            )
         }
-//        success {
-//            mail (to: "weipeng4@lenovo.com,cuixf1@lenovo.com",
-//                    subject: "Jenkins Auto Mail:${JOB_NAME}-${MANIFESTS} build SUCCESS!",
-//                    body: "The job ${JOB_NAME}-${BUILD_ID}-${MANIFESTS} BUILD SUCCESS.It's move to ${iso_dir}.you can open at ${url}");
-//        }
     }
 }
 
